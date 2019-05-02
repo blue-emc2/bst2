2a438e1f0ff49dee39d7d22f2deea269721299a3
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-05-02 15:32:55 +0900

    ヘッダーを追加

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Button, Container, Header, Icon, Segment } from 'semantic-ui-react';
 import StroyList, { Story } from './components/StoryList';
 
 const App: React.FC = () => {
@@ -14,14 +15,36 @@ const App: React.FC = () => {
       header: 'Matthew',
       description: 'Matthew is a musician living in Nashville.',
     },
+    {
+      id: 3,
+      header: 'Matthew',
+      description: 'Matthew is a musician living in Nashville.',
+    },
+    {
+      id: 4,
+      header: 'Matthew',
+      description: 'Matthew is a musician living in Nashville.',
+    },
   ];
 
   return (
     <div className="container">
-      <header>
-        <h1>back story</h1>
-      </header>
       <main>
+        <Segment
+          inverted
+          textAlign="center"
+          style={{ minHeight: 400, padding: '1em 0em' }}
+          vertical
+        >
+          <Container text>
+            <Header as="h1" inverted content="back story" />
+            <Button primary size="huge">
+              ストーリーを書いてみる
+              <Icon name="arrow right" />
+            </Button>
+          </Container>
+        </Segment>
+
         <StroyList stories={stories} />
       </main>
     </div>
