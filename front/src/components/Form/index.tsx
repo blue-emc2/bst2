45a467e58f94384d1453a6d0a7f0d888820a8004
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-05-16 19:36:41 +0900

    cosme

--- front/src/components/Form/index.tsx
+++ front/src/components/Form/index.tsx
@@ -2,22 +2,24 @@ import React, { FC } from 'react';
 import { Form, Button, Checkbox, Container } from 'semantic-ui-react';
 
 const StoryForm: FC<{}> = () => {
-  return (<Container style={{ marginTop: '7em' }}>
-    <Form>
-      <Form.Field>
-        <label>First Name</label>
-        <input placeholder='First Name' />
-      </Form.Field>
-      <Form.Field>
-        <label>Last Name</label>
-        <input placeholder='Last Name' />
-      </Form.Field>
-      <Form.Field>
-        <Checkbox label='I agree to the Terms and Conditions' />
-      </Form.Field>
-      <Button type='submit'>Submit</Button>
-    </Form>
-  </Container>);
+  return (
+    <Container style={{ marginTop: '7em' }}>
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder="First Name" />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder="Last Name" />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label="I agree to the Terms and Conditions" />
+        </Form.Field>
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default StoryForm;
