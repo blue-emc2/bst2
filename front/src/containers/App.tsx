2479c9f4ef298035f2149cc2e3e4d164a074bb79
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-05-28 10:41:25 +0900

    cosme

--- front/src/containers/App.tsx
+++ front/src/containers/App.tsx
@@ -5,7 +5,6 @@ export const useLayoutStore = (): ((layout: {}) => void) => {
   const layoutRef = useRef({});
 
   const store = (layout: {}) => {
-    console.info('store callback layout=', layout);
     layoutRef.current = layout;
   };
 
