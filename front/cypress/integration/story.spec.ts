f787b3a72de2bf156f72eaadb38e273b8ad5d4fc
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-08 00:42:46 +0900

    frontのテストを修正

--- front/cypress/integration/story.spec.ts
+++ front/cypress/integration/story.spec.ts
@@ -3,12 +3,10 @@ describe('/preview', () => {
 
   // TODO: 今はテストデータを表示しているが、サーバーから取るようにしたらstubとかにしないといけない
   describe('存在するストーリーにアクセスしたとき', () => {
-    const charactername = 'アムロ・レイ';
-    const username = 'hoge';
-    const text0 =
-      '小学校に居る時分学校の二階から飛び降りて一週間程腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。';
-    const text1 =
-      '親類のものから西洋製のナイフを貰つて奇麗な刃を日に翳して、友達に見せて居たら、';
+    const charactername = "Yoshi'p Sampo";
+    const username = 'Yoshi';
+    const text0 = '吾輩（わがはい）は猫である。名前はまだ無い。';
+    const text1 = 'どこで生れたかとんと見当（けんとう）がつかぬ。';
 
     beforeEach(() => {
       cy.visit(`${baseUrl}/story/1`);
@@ -32,23 +30,22 @@ describe('/preview', () => {
     });
   });
 
-  // TODO: バックエンドと通信できるようになったらエラーケースを書く
-  // describe('存在しないストーリーにアクセスしたとき', () => {
-  //   it('Homeに戻される', () => {
-  //     cy.server();
-  //     cy.route({
-  //       method: 'GET',
-  //       url: '/story/999',
-  //       status: 404,
-  //       response: {
-  //         errors: {
-  //           status: 404,
-  //         },
-  //       },
-  //     });
+  describe('存在しないストーリーにアクセスしたとき', () => {
+    it('Homeに戻される', () => {
+      cy.server();
+      cy.route({
+        method: 'GET',
+        url: '/story/999',
+        status: 404,
+        response: {
+          errors: {
+            status: 404,
+          },
+        },
+      });
 
-  //     cy.visit(`${baseUrl}/story/999`);
-  //     cy.location('pathname').should('eq', '/');
-  //   });
-  // });
+      cy.visit(`${baseUrl}/story/999`);
+      cy.location('pathname').should('eq', '/');
+    });
+  });
 });
