d2e4e78fe7d8c94852c3366143a2f04143634fec
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-09 20:50:58 +0900

    テストデータ追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -17,3 +17,24 @@ sections = [
 ]
 
 Story.create(character_name: "Yoshi'p Sampo", user_name: "Yoshi", sections: sections)
+
+attr = {
+  character_name: "マリオ",
+  user_name: "宮本茂",
+  bodies: [
+    "帽子とオーバーオールを着用し、鼻の下に髭を生やしている。こうしたデザインは、16x16のドットでキャラクターを表現するという
+    『ドンキーコング』当時の制約の中で視認性を重視した結果生まれたものである。
+    宮本茂による初期作品のデザインではシャツが青色でオーバーオールが赤色となっていたが、小田部羊一が後にデザインした『スーパーマリオブラザーズ3』以降
+    （日本国外では『Super Mario Bros. 2』（日本国内における『スーパーマリオUSA』）以降）は初期とは逆にシャツが赤色でオーバーオールが青色の配色となり、
+    現行のデザインでも後者の配色が主に用いられている。また、作品の内容や場面によっては普段のそれとは違う服装を身につけていることもある。",
+    "初登場した当時は正式な名前が決まっておらず、「ジャンプマン」「救助マン」「ミスター・ビデオゲーム」といった通称で呼ばれていた",
+    "双子の弟「ルイージ」がいる。",
+  ],
+}
+
+sections = []
+attr[:bodies].each do |b|
+  t = Text.create(body: b)
+  sections << Section.create(layout_type: "Text", text: t)
+end
+Story.create(character_name: attr[:character_name], user_name: attr[:user_name], sections: sections)
