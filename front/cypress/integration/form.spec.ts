0398145b2659d7410900a6c0b90ba1b56860a31c
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-13 18:56:04 +0900

    テストデータを本番に近づける

--- front/cypress/integration/form.spec.ts
+++ front/cypress/integration/form.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 describe('stories#create request', () => {
   const baseUrl = Cypress.env('baseUrl');
   const backendBaseUrl = Cypress.env('backendUrl');
@@ -8,13 +9,29 @@ describe('stories#create request', () => {
       method: 'POST',
       body: {
         story: {
-          title: '大地くん',
+          character_name: 'ルイージ',
+          user_name: '宮本茂',
+          sections_attributes: [
+            {
+              layout_type: 'Text',
+              // text: {
+              //   body:
+              //     '任天堂の看板キャラクター「マリオ」の双子の弟。兄のマリオより長身で痩せ型、カイゼル髭、シャツ・帽子が緑でオーバーオールが紺色、帽子のマークが「L」であるといった相違点がある。',
+              // },
+            },
+            {
+              layout_type: 'Text',
+              // text: {
+              //   body: '性格はマリオより穏やかで物静か。',
+              // },
+            },
+          ],
         },
       },
     }).then(response => {
       expect(response.status).to.eq(201);
 
-      const newUrl = `${baseUrl}/${response.body}`;
+      const newUrl = `${baseUrl}/story/${response.body}`;
       cy.visit(newUrl);
       cy.location('pathname').should('eq', newUrl);
     });
