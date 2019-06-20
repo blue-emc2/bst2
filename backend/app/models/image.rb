245cb327113a544c25ef25cade82fadaef0c8be6
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-20 20:03:32 +0900

    画像のURLを発行できるようにする

--- backend/app/models/image.rb
+++ backend/app/models/image.rb
@@ -1,3 +1,8 @@
 class Image < ApplicationRecord
-  has_one_attached :illustration
+  include Rails.application.routes.url_helpers
+  has_one_attached :body
+
+  def body_url
+    body.attached? ? url_for(body) : nil
+  end
 end
--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -14,12 +14,12 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -44,7 +44,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
@@ -52,3 +51,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Rails.application.routes.default_url_options[:host] = "localhost"
+Rails.application.routes.default_url_options[:port] = 5000
