fee476afd2c5d2f3a7636584764f48d5028a1e2a
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-25 23:15:09 +0900

    実行時エラーを修正

--- front/server.js
+++ front/server.js
@@ -13,7 +13,7 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
