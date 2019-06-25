38e38d89dffadb42fbf7608e16230219049f379c
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-25 23:15:51 +0900

    実行環境によってAPIのURLを切り替えるようにした

--- front/src/containers/APIConfig.ts
+++ front/src/containers/APIConfig.ts
@@ -4,6 +4,9 @@ export interface ApiConfig {
 }
 
 export const DEFAULT_API_CONFIG: ApiConfig = {
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? 'https://pacific-sands-39437.herokuapp.com/api/v1/'
+      : 'http://localhost:5000/api/v1/',
   timeout: 7000,
 };
