2114a14a3dff45651015f3ffb12c4469968b3bff
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2019-06-29 17:34:16 +0900

    localhost:3000 を復活

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "localhost:3001", "bst2-front.herokuapp.com"
+    origins "localhost:8080", "localhost:3000", "localhost:3001", "bst2-front.herokuapp.com"
 
     resource "*",
       headers: :any,
